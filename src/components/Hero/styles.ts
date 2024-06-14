2db75261b3284a45cf8f2673d59428d7cc5cfd38
kauan almeida <kauansouzapoielli@gmail.com>
2024-06-14 12:58:54 -0300

    convertendo css para styled componentes

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,4 +1,6 @@
-.form {
+import styled from 'styled-components'
+
+const Form = styled.div`
   height: 360px;
   width: 100%;
   background-image: url('https://cdn.pixabay.com/photo/2018/08/10/15/45/woman-3597101_1280.jpg');
@@ -6,9 +8,9 @@
   position: relative;
   display: flex;
   align-items: center;
-}
+`
 
-.form::before {
+const FormOverlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -17,25 +19,20 @@
   background-color: var(--cor-principal);
   content: '';
   opacity: 0.7;
-}
+`
 
-.form div {
+const FormContent = styled.div`
   position: relative;
   color: #eee;
-}
+`
 
-.heroTitle {
+const HeroTitle = styled.h1`
   font-family: Gloock, serif;
   font-size: 48px;
-}
 
-@media (max-width: 768px) {
-  .form {
-    height: auto;
-    padding: 24px 0;
-  }
-
-  .heroTitle {
+  @media (max-width: 768px) {
     font-size: 32px;
   }
-}
+`
+
+export { Form, FormOverlay, FormContent, HeroTitle }
